28ac418d159514832b6bfd406d03638e1539c7e4
Fantasty9413 <969197213@qq.com>
2020-10-20 19:05:28 +0800

    add this pointer

---
+++ test/class_32.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+enum stusex
+{
+    male = 0 ,
+    female = 1
+};
+
+class student
+{
+    private:
+    string name;
+    int num;
+    stusex sex;
+    int age;
+
+    public:
+    void set_information();
+    void print_information();
+
+    void set_age(int age)
+    {
+        this->age = age;
+    }
+
+};
+
+    void student::set_information()
+    {
+        cout<<"please cin the name: "<<endl;
+        cin>>name;
+        cout<<endl;
+
+        cout<<"please cin the num: "<<endl;
+        cin>>num;
+        cout<<endl;
+
+        cout<<"please cin the sex: "<<endl;       
+        int stusex;
+        cin>>stusex;
+        sex = stusex == 1 ? female : male;
+        cout<<endl;
+
+        cout<<"please cin the age: "<<endl;            
+        cin>>age;
+        cout<<endl;
+    }
+
+    void student::print_information()
+    {
+        cout<<"name = "<<name<<endl;
+        cout<<"num = "<<num<<endl;
+        cout<<"sex = "<< (sex == 0 ? "male" : "female") <<endl;
+        cout<<"age = "<<age<<endl;
+        cout<<endl;
+    }
+
+int main()
+{
+    // student zhangsan = {"zhangsan", 101, male, 21};
+
+    student lisi; 
+    lisi.set_information();
+    lisi.print_information();
+
+    lisi.set_age(25);
+    lisi.print_information();
+
+    return 0;
+}
