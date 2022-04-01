5bb56173de1c366ddcbecdffa5045dad1af20326
Fantasty9413 <969197213@qq.com>
2022-04-01 22:47:08 +0800

    add reverse left words

---
+++ leetcode/string/ReverseLeftWords.cpp
@@ -0,0 +1,15 @@
+# include <iostream>
+# include <string>
+# include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    string reverseLeftWords(string s, int n) 
+    {
+        reverse(s.begin(), s.begin()+n);
+        reverse(s.begin()+n, s.end());
+        reverse(s.begin(), s.end());
+        return s;
+    }
+};
